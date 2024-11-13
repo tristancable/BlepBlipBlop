18c61bf2b9d134af59230cbcbd677fd9212d60df
Darrian Redford <dredford@student.neumont.edu>
2024-11-12 18:08:30 -0700

    Compacted the preset files for a cleaner look

--- API/src/main/java/com/example/BlepBlipBlop/BlepBlipBlopApplication.java
+++ API/src/main/java/com/example/BlepBlipBlop/BlepBlipBlopApplication.java
@@ -1,13 +1,9 @@
 package com.example.BlepBlipBlop;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
 public class BlepBlipBlopApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(BlepBlipBlopApplication.class, args);
 	}
-
 }
