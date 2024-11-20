a7751cd0d348296da723036c019720b1e83a784f
ezeklaww <eandreason@student.neumont.edu>
2024-11-20 16:36:09 -0700

    added the base for jpa

--- API/src/main/java/com/example/BlepBlipBlop/Models/User.java
+++ API/src/main/java/com/example/BlepBlipBlop/Models/User.java
@@ -1,8 +1,26 @@
 package com.example.BlepBlipBlop.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "User")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
     private String username;
+
+    @Column(nullable = false)
     private String password;
+
+    @Column(nullable = false)
     private byte level;
     
     public User(String username, String password, byte level) {
