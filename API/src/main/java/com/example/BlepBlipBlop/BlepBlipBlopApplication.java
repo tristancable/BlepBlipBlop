b645a0dce08f6b277b09ce5c08bc78371a86eba6
Darrian Redford <dredford@student.neumont.edu>
2024-11-21 17:10:29 -0700

    Added cors stuff

--- API/src/main/java/com/example/BlepBlipBlop/BlepBlipBlopApplication.java
+++ API/src/main/java/com/example/BlepBlipBlop/BlepBlipBlopApplication.java
@@ -1,9 +1,21 @@
 package com.example.BlepBlipBlop;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class BlepBlipBlopApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BlepBlipBlopApplication.class, args);
+	}
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8081").allowedMethods("*");
+			}
+		};
 	}
 }
