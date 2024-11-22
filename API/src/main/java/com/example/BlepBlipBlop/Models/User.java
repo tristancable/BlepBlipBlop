9d7355f405b41c0925d506524b32970df40a2afe
ezeklaww <eandreason@student.neumont.edu>
2024-11-21 19:20:12 -0700

    moved the database stuff just into the controllers

--- API/src/main/java/com/example/BlepBlipBlop/Models/User.java
+++ API/src/main/java/com/example/BlepBlipBlop/Models/User.java
@@ -1,10 +1,14 @@
 package com.example.BlepBlipBlop.Models;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
+@Entity
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
